0d262da4fd0f53d92620a48e6e749233d651ae94
Silvio Limeira <silvio.limeira@live.com>
2019-05-23 19:44:55 -0300

    sample use npx ts-node for execute .ts files

---
+++ src/assemble.ts
@@ -0,0 +1,9 @@
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+const appService = new AppService();
+const appController = new AppController(appService);
+const hello = appController.getHello();
+
+console.log(hello);
+// execute: npx ts-node ./src/assemble.ts
