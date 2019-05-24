88b2409129c993b6b08b5181c5cea08a73c078b4
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-05-24 07:45:13 -0300

    Provide cors options from app.enableCors()

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Provide cors options for app.enableCors()
+  //   We should be able to set cors options, e.g:
+  // links: https://github.com/nestjs/nest/issues/457
+  //          https://github.com/expressjs/cors#configuration-options  
+  app.enableCors({
+    allowedHeaders: ['Location']
+  });
+
   await app.listen(3000);
 }
 bootstrap();
