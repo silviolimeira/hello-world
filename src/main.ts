4a7714d9588d354ccbfb34b927f97e6402c881b3
Silvio Limeira <silvio.limeira@live.com>
2019-05-24 19:47:41 -0300

    setting up a template engine

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+//  const APP_DIRECTORY = resolve(__dirname, '..');
+
+//  const app = await NestFactory.create(AppModule);
+
   // Provide cors options for app.enableCors()
   //   We should be able to set cors options, e.g:
   // links: https://github.com/nestjs/nest/issues/457
   //          https://github.com/expressjs/cors#configuration-options  
+//  app.enableCors({
+//    allowedHeaders: ['Location']
+//  });
+
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   app.enableCors({
     allowedHeaders: ['Location']
   });
